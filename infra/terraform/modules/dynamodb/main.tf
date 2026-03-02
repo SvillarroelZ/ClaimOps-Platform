5f66669d0555d7bfc5104a91ccf7f9a869171301
Sofia Villarroel Z <102156826+SvillarroelZ@users.noreply.github.com>
2026-03-02 05:34:10 +0000

    feat: add DynamoDB module with PAY_PER_REQUEST billing mode

---
+++ infra/terraform/modules/dynamodb/main.tf
@@ -0,0 +1,34 @@
+resource "aws_dynamodb_table" "main" {
+  name           = var.table_name != "" ? var.table_name : "${var.project_name}-events"
+  billing_mode   = var.billing_mode
+  hash_key       = var.partition_key
+  range_key      = var.sort_key
+  stream_enabled = true
+
+  attribute {
+    name = var.partition_key
+    type = "S"
+  }
+
+  attribute {
+    name = var.sort_key
+    type = "S"
+  }
+
+  point_in_time_recovery {
+    enabled = var.enable_point_in_time_recovery
+  }
+
+  dynamic "ttl" {
+    for_each = var.enable_ttl ? [1] : []
+    content {
+      attribute_name = var.ttl_attribute
+      enabled        = true
+    }
+  }
+
+  tags = {
+    Name        = var.project_name
+    Environment = var.environment
+  }
+}
